ba925adbd03b7a99dd61734f3970f0154c47571d
Deny Sufriawan <deny.sufriawan@mitrais.com>
2017-09-05 23:00:22 +0800

    Add Lib Semantic,Sweetalert2,JQuery || Add Component for each UI and simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PeriodComponent } from './period/period.component';
+import { UserComponent } from './user/user.component';
+import { EnrollmentComponent } from './enrollment/enrollment.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { TrainingComponent } from './training/training.component';
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './template/sidebar/sidebar.component';
+import { MenuComponent } from './template/menu/menu.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent},
+  { path: 'period', component: PeriodComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'enrollment', component: EnrollmentComponent },
+  { path: 'achievement', component: AchievementComponent },
+  { path: 'training', component: TrainingComponent },
+  { path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PeriodComponent,
+    UserComponent,
+    EnrollmentComponent,
+    AchievementComponent,
+    TrainingComponent,
+    LoginComponent,
+    SidebarComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
